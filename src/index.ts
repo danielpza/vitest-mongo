d92f4821292d886c4024961c177e4604db824b6d
Daniel Perez <danielpza@protonmail.com>
2026-05-10 00:14:58 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export default function vitestMongo() {
-  return null;
+import type { MongoMemoryServer } from "mongodb-memory-server";
+
+import type {} from "./globalSetup.ts";
+
+type MongoMemoryServerOpts = Parameters<typeof MongoMemoryServer.create>[0];
+
+declare module "vitest/node" {
+  export interface ResolvedConfig {
+    vitestMongo?: {
+      mongodbMemoryServerOptions?: MongoMemoryServerOpts;
+    };
+  }
+}
+
+export default function vitestMongo(opts?: { mongodbMemoryServerOptions?: MongoMemoryServerOpts }) {
+  return {
+    name: "vitest-mongo",
+    config: () => ({
+      vitestMongo: opts,
+      test: {
+        globalSetup: [import.meta.resolve("./globalSetup.mjs")],
+      },
+    }),
+  };
 }
