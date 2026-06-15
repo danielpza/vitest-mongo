d57f6a61c4699118498c2aaefd624d3b5ecfd9a5
Daniel Perez <danielpza@protonmail.com>
2026-06-15 13:26:58 -0700

    some comments

--- tests/src/index.test.ts
+++ tests/src/index.test.ts
@@ -6,15 +6,18 @@ test("expect inject('MONGO_URI') to be defined", () => {
 });
 
 test("mongodb", async ({ onTestFinished }) => {
+  // arrange
   const client = new MongoClient(inject("MONGO_URI"));
   await client.connect();
   onTestFinished(async () => {
     await client.db("test").dropDatabase();
     await client.close();
   });
+  // act
   await client.db("test").collection("test").insertOne({
     foo: "bar",
   });
+  // assert
   expect(await client.db("test").collection("test").findOne({})).toEqual({
     _id: expect.any(ObjectId),
     foo: "bar",
