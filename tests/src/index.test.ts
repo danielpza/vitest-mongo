97022770e5ad5f5b94b89ac36ced78d970e43b21
Daniel Perez <danielpza@protonmail.com>
2026-06-15 13:25:57 -0700

    add mongodb test

--- tests/src/index.test.ts
+++ tests/src/index.test.ts
@@ -1,5 +1,22 @@
 import { expect, inject, test } from "vitest";
+import { MongoClient, ObjectId } from "mongodb";
 
 test("expect inject('MONGO_URI') to be defined", () => {
   expect(inject("MONGO_URI")).toBeDefined();
+});
+
+test("mongodb", async ({ onTestFinished }) => {
+  const client = new MongoClient(inject("MONGO_URI"));
+  await client.connect();
+  onTestFinished(async () => {
+    await client.db("test").dropDatabase();
+    await client.close();
+  });
+  await client.db("test").collection("test").insertOne({
+    foo: "bar",
+  });
+  expect(await client.db("test").collection("test").findOne({})).toEqual({
+    _id: expect.any(ObjectId),
+    foo: "bar",
+  });
 });
