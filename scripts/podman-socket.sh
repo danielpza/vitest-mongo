967f6a16b6cdb59a5c6ad3a4f4140ac378728291
Daniel Perez <danielpza@protonmail.com>
2026-06-15 16:22:53 -0700

    Delete podman-socket.sh

--- scripts/podman-socket.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# see https://node.testcontainers.org/supported-container-runtimes/#podman
-# see https://podman-desktop.io/tutorial/testcontainers-with-podman
-
-export DOCKER_HOST=unix://$(
-  podman info --format '{{.Host.RemoteSocket.Path}}'
-)
